14819ca82fd075002f2975f516bd31db5fcab61f
Stanislav Samulinas <samulinas@gmail.com>
2023-12-13 13:20:37 +0200

    Добавьте файлы проекта.

---
+++ AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Category { get; set; }
+
+    //public DbSet<Book> Book { get; set; }
+}
